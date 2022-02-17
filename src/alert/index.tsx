ed92ae4023414721918f0e2b1214e60a68cfbef2
xiaodan <526131424@qq.com>
2022-02-17 15:11:59 +0800

    feat: change package name

---
+++ src/alert/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export interface AlertProps {
+  /**
+   * @description       Alert 的类型
+   * @default           'info'
+   */
+  kind?: 'info' | 'positive' | 'negative' | 'warning';
+}
+
+export type KindMap = Record<Required<AlertProps>['kind'], string>;
+
+const prefixCls = 'happy-alert';
+
+const kinds: KindMap = {
+  info: '#5352E1',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert: React.FC<AlertProps> = ({ children, kind = 'info', ...rest }) => (
+  <div
+    className={prefixCls}
+    style={{
+      background: kinds[kind],
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+// Alert.propTypes = {
+//   kind: t.oneOf(['info', 'positive', 'negative', 'warning']),
+// };
+
+export default Alert;
