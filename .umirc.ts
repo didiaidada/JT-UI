5297e29a45cd2256966c79bb12a9f367faa9c2fd
xiaodan <526131424@qq.com>
2022-02-18 17:38:30 +0800

    feat: delete useless

--- .umirc.ts
+++ .umirc.ts
@@ -6,4 +6,6 @@ export default defineConfig({
   outputPath: 'doc-site', // 输出文件夹
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+  base:'https://github.com/didiaidada/cmri-ui',
+  publicPath: '/cmri-ui/',
 })
