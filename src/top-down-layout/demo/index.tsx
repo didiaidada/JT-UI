b94950d3eb43393dad62b8ea5871cb1bac529a3e
xiaodan <526131424@qq.com>
2022-03-16 11:08:11 +0800

    feat: 添加多个组件 SearchInput，Breadcrumb，基础布局

--- src/top-down-layout/demo/index.tsx
+++ src/top-down-layout/demo/index.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import TopBottomLayout from '..';
+import Button from '../../button';
 import './index.less';
 
-const showSearch = {
-  placeholder: '点击回车搜索',
-  onPressEnter: (e: React.KeyboardEvent<HTMLInputElement>) => console.log(e),
-};
+// const showSearch = {
+//   placeholder: '点击回车搜索',
+//   onPressEnter: (e: React.KeyboardEvent<HTMLInputElement>) => console.log(e),
+// };
 
 const pathList = [
   { label: 'label1', url: '' },
   { label: 'button', url: '/button' },
 ];
+const extra = () => (
+  <Button type="primary" size="big">
+    确认
+  </Button>
+);
 
 const breadcrumb = {
   pathList,
+  extra: extra(),
 };
 export default () => (
   <div className="demo-window-out">
     <div className="demo-window">
-      <TopBottomLayout title="测试标题" showSearch={showSearch} breadcrumb={breadcrumb} />
+      <TopBottomLayout title="测试标题" breadcrumb={breadcrumb} />
     </div>
   </div>
 );
